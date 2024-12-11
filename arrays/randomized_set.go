fbc53e8622598941653468b89ba9bb373fee3331
Tikhon Sakharov <tikhon@Tikhons-MacBook-Pro.local>
2024-12-11 23:00:16 +0300

    Randomized set: implemented hash set

--- arrays/randomized_set.go
+++ arrays/randomized_set.go
@@ -7,32 +7,55 @@ import (
 	"golang.org/x/exp/rand"
 )
 
+const (
+	MaxUint        = ^uint32(0)
+	MinUint32      = 0
+	MaxInt32       = int32(MaxUint >> 1)
+	MinInt32       = -MaxInt32 - 1
+	ValuesRange    = int(MaxInt32) - int(MinInt32) + 1
+	CountOfBuckets = 10000
+	BucketStep     = int(ValuesRange / CountOfBuckets)
+)
+
 type RandomizedSet struct {
-	elements map[int]int
+	countOfBuckets int
+	elements       map[int][]int
 }
 
 func Constructor() RandomizedSet {
 	return RandomizedSet{
-		elements: make(map[int]int),
+		countOfBuckets: CountOfBuckets,
+		elements:       make(map[int][]int),
 	}
 }
 
 func (rs *RandomizedSet) Insert(val int) bool {
-	if rs.contains(val) {
-		return false
+	bucket, hasBucket := rs.elements[rs.Hash(val)]
+	if hasBucket {
+		if rs.isBucketContainsValue(bucket, val) {
+			return false
+		}
+		rs.elements[rs.Hash(val)] = append(bucket, val)
+		return true
+	} else {
+		rs.elements[rs.Hash(val)] = []int{val}
+		return true
 	}
-
-	rs.elements[val] = val
-	return true
 }
 
 func (rs *RandomizedSet) Remove(val int) bool {
-	if rs.contains(val) {
-		return false
+	bucket, hasBucket := rs.elements[rs.Hash(val)]
+	if hasBucket {
+		for i, v := range bucket {
+			if v == val {
+				bucket[i] = bucket[len(bucket)-1]
+				rs.elements[rs.Hash(val)] = bucket[:len(bucket)-1]
+				return true
+			}
+		}
 	}
 
-	delete(rs.elements, val)
-	return true
+	return false
 }
 
 func (rs *RandomizedSet) GetRandom() int {
@@ -40,25 +63,53 @@ func (rs *RandomizedSet) GetRandom() int {
 	maxIndex := len(rs.elements)
 	fmt.Println(maxIndex)
 	index := rand.Intn(maxIndex-0) + maxIndex
-	return rs.pick(index)
+	return index
 }
 
-func (rs *RandomizedSet) contains(val int) bool {
-	_, ok := rs.elements[val]
+func (rs *RandomizedSet) Contains(val int) bool {
+	bucket, ok := rs.elements[rs.Hash(val)]
 	if ok {
-		return true
+		return rs.isBucketContainsValue(bucket, val)
 	}
 	return false
 }
 
-func (rs *RandomizedSet) pick(index int) int {
-	for _, v := range rs.elements {
-		if index == 0 {
-			return v
+func (rs *RandomizedSet) isSetContainsBucket(val int) bool {
+	_, ok := rs.elements[rs.Hash(val)]
+	return ok
+}
+
+func (rs *RandomizedSet) isBucketContainsValue(bucket []int, val int) bool {
+	for _, v := range bucket {
+		if v == val {
+			return true
 		}
-		index--
 	}
-	return -1
+	return false
+}
+
+func (rs *RandomizedSet) Hash(val int) int {
+	return val / BucketStep
+}
+
+func (rs *RandomizedSet) GetValue(val int) int {
+	bucket, hasBucket := rs.elements[rs.Hash(val)]
+	if hasBucket {
+		for _, v := range bucket {
+			if v == val {
+				return val
+			}
+		}
+	}
+
+	return 505
+}
+
+func (rs *RandomizedSet) PrintVals(vals []int) {
+	fmt.Printf("\n\n==== RandomizedSet =====")
+	for _, v := range vals {
+		fmt.Printf("\nval %d, hash %d", rs.GetValue(v), rs.Hash(v))
+	}
 }
 
 /**
